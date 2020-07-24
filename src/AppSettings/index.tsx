cb44f0bacc5b077d2039d1dc137847e3defbf749
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-07-24 21:20:57 +0300

    Add basic settings section

---
+++ src/AppSettings/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "@adobe/react-spectrum";
+
+export const AppSettings = () => {
+  return (
+    <View
+      margin="size-200"
+      padding="size-200"
+      backgroundColor="gray-200"
+      width="size-2000"
+    ></View>
+  );
+};
